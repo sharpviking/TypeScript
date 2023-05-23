ce7f4c0278aa4a2bffe26f0bf96a46f63f23be4e
Abhishek Anand <anandabhishek3607@gmail.com>
2023-05-23 16:07:40 +0530

    typescript basics

---
+++ typescript.js
@@ -0,0 +1,5 @@
+var addition = function (a, b) {
+    return a + b;
+};
+var answer = addition("26", 11);
+console.log(answer);
---
+++ typescript.ts
@@ -0,0 +1,6 @@
+const addition = (a: number, b: number) => {
+  return a + b;
+};
+const answer = addition("26", 11);
+
+console.log(answer);
