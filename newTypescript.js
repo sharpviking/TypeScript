094a9ecdf22bc71163407ebf234afc0296d94a57
Abhishek Anand <anandabhishek3607@gmail.com>
2023-05-24 12:16:55 +0530

    24th May commit

---
+++ newTypescript.js
@@ -0,0 +1,16 @@
+var isCool = true;
+//number
+var age = 56;
+//string
+var eyeColor = "blue";
+var favoriteQuote = "I'm not old.i'm only ".concat(age);
+//Array
+var pets = ["cat", "dog", "pig"];
+var pets2 = ["dragon", "Ibex", "MonkeySpider"];
+//Objects
+var wizard = {
+    a: "John",
+};
+//null and undefined
+var meh = undefined;
+var nah = null;
---
+++ newTypescript.ts
@@ -0,0 +1,21 @@
+let isCool: boolean = true;
+
+//number
+let age: number = 56;
+
+//string
+let eyeColor: string = "blue";
+let favoriteQuote: string = `I'm not old.i'm only ${age}`;
+
+//Array
+let pets: string[] = ["cat", "dog", "pig"];
+let pets2: Array<string> = ["dragon", "Ibex", "MonkeySpider"];
+
+//Objects
+let wizard: object = {
+  a: "John",
+};
+
+//null and undefined
+let meh: undefined = undefined;
+let nah: null = null;
