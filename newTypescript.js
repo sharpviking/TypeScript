6d27f7ae249ba13686d718e94e7647c195eea4a0
Abhishek Anand <anandabhishek3607@gmail.com>
2023-05-24 14:57:40 +0530

    TYpes in typescript

--- newTypescript.js
+++ newTypescript.js
@@ -14,3 +14,57 @@ var wizard = {
 //null and undefined
 var meh = undefined;
 var nah = null;
+//Tuple
+var basket;
+basket = ["Basketball", 26111];
+//Enum
+var Size;
+(function (Size) {
+    Size[Size["Small"] = 1] = "Small";
+    Size[Size["Medium"] = 2] = "Medium";
+    Size[Size["Large"] = 3] = "Large";
+})(Size || (Size = {}));
+var sizeName = Size.Small;
+//Any - !!!!!!!! Be Careful
+var whatEver = "avoooooiiid thiiiiiis!!!";
+whatEver = "looper";
+//void
+var chant = function () {
+    console.log("oooooommmmmmmmmm");
+};
+// never
+var error = function () {
+    throw Error("oooopss");
+};
+var fightrobotArmy = function (robots) {
+    console.log("Fight");
+};
+var fightrobotArmy3 = function (robots) {
+    console.log("Fight");
+};
+var fightrobotArmy2 = function (robots) {
+    console.log("Fight");
+};
+var dog = {};
+dog.count;
+//Function
+var fightrobotArmy4 = function (robots) {
+    console.log("Fight!");
+};
+var fightrobotArmy5 = function (robots) {
+    console.log("FIGHT!");
+    return 26;
+};
+//Classes
+var Animal = /** @class */ (function () {
+    function Animal(sound) {
+        this.sing = "lalalalal hohoho ho";
+        this.sing = sound;
+    }
+    Animal.prototype.greet = function () {
+        return "Hello ".concat(this.sing);
+    };
+    return Animal;
+}());
+var lion = new Animal("Dahaaaar");
+lion.sing;
--- newTypescript.ts
+++ newTypescript.ts
@@ -19,3 +19,99 @@ let wizard: object = {
 //null and undefined
 let meh: undefined = undefined;
 let nah: null = null;
+
+//Tuple
+let basket: [string, number];
+basket = ["Basketball", 26111];
+
+//Enum
+enum Size {
+  Small = 1,
+  Medium = 2,
+  Large = 3,
+}
+let sizeName: number = Size.Small;
+
+//Any - !!!!!!!! Be Careful
+let whatEver: any = "avoooooiiid thiiiiiis!!!";
+whatEver = "looper";
+
+//void
+let chant = (): void => {
+  console.log("oooooommmmmmmmmm");
+};
+
+// never
+let error = (): never => {
+  throw Error("oooopss");
+};
+
+//interface
+interface RobotArmy {
+  count: number;
+  type: string;
+  magic: string;
+}
+
+let fightrobotArmy = (robots: RobotArmy) => {
+  console.log("Fight");
+};
+
+interface RobotArmy3 {
+  count: number;
+  type: string;
+  magic?: string; // "?" make it optional.
+}
+
+let fightrobotArmy3 = (robots: RobotArmy) => {
+  console.log("Fight");
+};
+
+type RobotArmy2 = {
+  count: number;
+  type: string;
+  magic: string;
+};
+
+let fightrobotArmy2 = (robots: RobotArmy) => {
+  console.log("Fight");
+};
+
+//Type Assertion
+interface catArmy {
+  count: number;
+  type: string;
+  magic: string;
+}
+
+let dog = {} as catArmy;
+dog.count;
+
+//Function
+let fightrobotArmy4 = (robots: RobotArmy): void => {
+  console.log("Fight!");
+};
+
+let fightrobotArmy5 = (robots: {
+  count: number;
+  type: string;
+  magic: string;
+}): number => {
+  console.log("FIGHT!");
+  return 26;
+};
+
+//Classes
+class Animal {
+  sing: string = "lalalalal hohoho ho";
+  constructor(sound: string) {
+    this.sing = sound;
+  }
+
+  greet() {
+    return `Hello ${this.sing}`;
+  }
+}
+
+let lion = new Animal("Dahaaaar");
+lion.sing;
